381e06b069b4bcc0f8b101cc2ab7661152276a4c
mucadlucca <mucael@gmail.com>
2019-11-25 15:37:40 -0300

    RSXP zero ao deploy

---
+++ index.js
@@ -0,0 +1,26 @@
+const koa = require ('koa')
+const Router = require('koa-router')
+
+const app = new koa()
+const router = new Router()
+
+const index = ctx => {
+  ctx.body = 'Node.js do zero ao deploy'
+}
+
+const hello = ctx => {
+  const name = ctx.params.name || 'World'
+  ctx.body = `Hello ${name}!`
+}
+
+router.get('/', index)
+router.get('/hello', hello)
+router.get('/hello/:name', hello)
+
+app.use(router.routes())
+
+const port = 3000
+
+app.listen(port)
+  .on('listening', () => console.log(`Listning on port ${port}`))
+
